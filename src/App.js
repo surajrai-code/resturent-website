86d37d63fee0de12b0f01c78b22af7a548ddd2d0
surajrai-code <surajraieng>
2023-03-13 10:24:56 +0530

    Add Meals item

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
+import MealList from './components/Meals/MealList';
 import MealsSummary from './components/Meals/MealsSummary';
 function App() {
   return (
     <Fragment>
       <Header/>
       <MealsSummary/>
+      <MealList></MealList>
     </Fragment>
   );
 }
