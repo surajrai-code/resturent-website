e189fb8c44ee3a7e4e09fa7c20ced7654b8dd9a6
surajrai-code <surajraieng>
2023-03-13 09:57:05 +0530

    adding meals summary

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
 function App() {
   return (
     <Fragment>
       <Header/>
+      <MealsSummary/>
     </Fragment>
   );
 }
